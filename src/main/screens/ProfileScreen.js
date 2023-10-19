f06980262783e38eccd255ca4024c0aaf185bbf0
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-10-19 17:33:08 +0530

    moment with weeks

--- src/main/screens/ProfileScreen.js
+++ src/main/screens/ProfileScreen.js
@@ -1,11 +1,71 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import moment from 'moment';
 
 const ProfileScreen = () => {
+  const [commObj, setCommObj] = useState({
+    dateWeeks: [],
+    todayDate: new Date(),
+  });
+
+  useEffect(() => {
+    getWeeksDatesHandler();
+  }, []);
+
+  function getWeeksDatesHandler() {
+    let currentDate = moment();
+    let weekArr = [];
+    for (let i = 0; i < 7; i++) {
+      let day = currentDate.clone().day(i);
+      weekArr.push({
+        dayName: day.format('ddd'),
+        date: day.format('YYYY-MM-DD'),
+      });
+    }
+
+    setCommObj(prev => ({
+      ...prev,
+      dateWeeks: weekArr,
+    }));
+  }
+
+  useEffect(() => {
+    console.log('commObj----->', commObj);
+  }, [commObj]);
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <View>
+          <FlatList
+            data={commObj.dateWeeks}
+            keyExtractor={item => item.date}
+            renderItem={({item}) => {
+              return (
+                <TouchableWithoutFeedback>
+                  <View>
+                    <Text>{item.dayName}</Text>
+                    <Text>{}</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              );
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
