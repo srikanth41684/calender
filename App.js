2e724545dfb33b1d813a2b73ebd788a18a02fe1d
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-10-30 17:13:14 +0530

    swipe-gestures i

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import {NavigationContainer} from '@react-navigation/native';
 import GlobalStackNav from './src/navigations/GlobalStackNav';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
-      <NavigationContainer>
-        <GlobalStackNav />
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <GlobalStackNav />
+    </NavigationContainer>
   );
 };
 
