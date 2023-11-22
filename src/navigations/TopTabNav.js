8e6f67d4ec161d9e33d993a5230086c0624c856d
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-11-22 14:41:47 +0530

    minimum date set

--- src/navigations/TopTabNav.js
+++ src/navigations/TopTabNav.js
@@ -344,6 +344,8 @@ const TopTabNav = props => {
             modal
             mode="date"
             date={commObj.fromDate}
+            minimumDate={new Date()}
+            maximumDate={new Date('2024-03-31')}
             open={commObj.formDatePicker}
             title="Select Start Date"
             onConfirm={date => {
